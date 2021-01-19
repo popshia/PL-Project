730e28185b48eeed3d11ab454ce3b4157f1159ca
popshia <b123292179@gmail.com>
2021-01-19 17:47:27 +0800

    Initial Commit

---
+++ PL_Project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  PL_Project
+//
+//  Created by 冠良 on 2021/1/19.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
