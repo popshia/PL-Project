e4ca8fc713e2399d82c06e3fe0e941ec9aad799f
popshia <b123292179@gmail.com>
2021-01-21 15:34:43 +0800

    cut token by spaces

--- PL_Project/main.cpp
+++ PL_Project/main.cpp
@@ -1,14 +1,52 @@
-//
 //  main.cpp
 //  PL_Project
-//
-//  Created by 冠良 on 2021/1/19.
-//
+
+// <ATOM>  ::= SYMBOL | INT | FLOAT | STRING | NIL | T | LEFT-PAREN RIGHT-PAREN
+// <S-exp> ::= <ATOM> | LEFT-PAREN <S-exp> { <S-exp> } [ DOT <S-exp> ] RIGHT-PAREN | QUOTE <S-exp>
 
 #include <iostream>
+#include <stdio.h>
+#include <string>
+using namespace std;
+
+typedef struct ExprStruct{
+    string expr = "\0";
+    string type = "\0";
+    ExprStruct* next = NULL;
+} ExprStruct; // single expr structure
+
+ExprStruct* ReadSExp() {
+    ExprStruct* head = NULL;
+    ExprStruct* newToken = new ExprStruct;
+    head = newToken;
+    while (cin.peek() != '\n') {
+        while (cin.peek() != ' ' && cin.peek() != '\n') {
+            newToken->expr += cin.get();
+        } // get one token
+        if (cin.peek() != '\n') {
+            cin.get();
+            newToken->next = new ExprStruct;
+            newToken = newToken->next;
+        } // get the spaces
+    }
+    return head;
+} // read the tokens
+
+void PrintSExpr(ExprStruct* head) {
+    ExprStruct* cursor = head;
+    while (cursor != NULL) {
+        cout << cursor->expr << endl;
+        cursor = cursor->next;
+    }
+} // print our the tokens stored in the pointer
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
+//    int uTestNum = 0;
+//    bool end = false;
+    cout << "Welcome to OurScheme!" << endl;
+//    do {
+    cout << "> ";
+    PrintSExpr(ReadSExp());
+//    } while (!end);
+    cout << endl <<"Thanks for using OurScheme!";
+} // main screen
